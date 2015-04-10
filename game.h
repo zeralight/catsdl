b1922ccbbcd9d7b0f3d907f2a9b38d78751531db
hedi ghediri <zeralight@users.noreply.github.com>
2015-04-10 21:28:17 +0100

    Create game.h

---
+++ game.h
@@ -0,0 +1,15 @@
+#ifndef GAME_H_INCLUDED
+#define GAME_H_INCLUDED
+#include <assert.h>
+#include <stdio.h>
+#include "graph.h"
+#include "common.h"
+#include "gfx.h"
+
+int xStart;
+int yStart;
+gameLevel gameMenu(bool *back);
+void advancePos(position* pos, unsigned lim);
+const position *nextAction(gameGraph *graph, const position *catPos, const gameMap *map);
+bool posLim(const position *p, size_t lim);
+#endif // GAME_H_INCLUDED
